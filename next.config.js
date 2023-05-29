cf3b21f3f1dbf6e1c370ae53b96b7ba892d529ed
Mindul <pencake33@naver.com>
2023-05-29 22:14:42 +0900

    최초 image 띄우기

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withImages = require('next-images');
+const nextConfig = {};
+
+module.exports = withImages(), nextConfig;
+
